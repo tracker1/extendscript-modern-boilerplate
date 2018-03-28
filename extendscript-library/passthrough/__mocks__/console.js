df8a291a2b8b0d567d9ad5d9a9aaa37d97e1f7df
Michael J. Ryan <tracker1@gmail.com>
2018-03-27 17:46:30 -0700

    fix test(s)

--- extendscript-library/passthrough/__mocks__/console.js
+++ extendscript-library/passthrough/__mocks__/console.js
@@ -1,7 +1,10 @@
-export default jest.fn().mockImplementation(() => ({
+const con = {
+  mockClear: () => Object.values(con).filter(fn => fn && fn.mockClear).forEach(fn => fn.mockClear()),
   log: jest.fn(),
   warn: jest.fn(),
   error: jest.fn(),
   info: jest.fn(),
   dir: jest.fn(),
-}));
+};
+
+export default con;
--- src/output.test.js
+++ src/output.test.js
@@ -8,7 +8,14 @@ import $ from 'helper';
 
 import sit from './output';
 
-test('output', () => {
+jest.mock('console');
+jest.mock('app');
+jest.mock('file');
+jest.mock('folder');
+jest.mock('window');
+jest.mock('helper');
+
+describe('output', () => {
   beforeEach(() => {
     console2.mockClear();
     app.mockClear();
@@ -18,12 +25,12 @@ test('output', () => {
     $.mockClear();
   });
 
-  if ('Will output as expected', () => {
+  it('Will output as expected', () => {
     sit();
-    expect(console2.log.calls[0][0]).toEqual('scope');
-    expect(console2.log.calls[0][1]).toEqual(0);
+    expect(console2.log.mock.calls[0][0]).toEqual('scope');
+    expect(console2.log.mock.calls[0][1]).toEqual(0);
 
-    expect(console2.log.calls[1][0]).toEqual('scope');
-    expect(console2.log.calls[1][1]).toEqual(1);
+    expect(console2.log.mock.calls[1][0]).toEqual('scope');
+    expect(console2.log.mock.calls[1][1]).toEqual(1);
   });
 });
